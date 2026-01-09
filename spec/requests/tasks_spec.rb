c68e93f57ceb46153c9e949cdcf68f6dde009bdf
sahil-incubyte <sahil@incubyte.co>
2026-01-09 13:32:44 +0530

    test: add request spec for GET /tasks/:id

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe "Tasks API", type: :request do
       expect(JSON.parse(response.body).size).to eq(3)
     end
   end
+
+  describe "GET /tasks/:id" do
+    let(:task) { create(:task) }
+
+    it "returns a single task" do
+      get "/tasks/#{task.id}"
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["id"]).to eq(task.id)
+    end
+  end
 end
