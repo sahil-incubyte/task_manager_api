1094cf0e6a62511d488f5a778f6057f097c7127b
sahil-incubyte <sahil@incubyte.co>
2026-01-09 13:38:42 +0530

    feat: add PATCH /tasks/:id endpoint

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,17 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    task = Task.find(params[:id])
+
+    if task.update(task_params)
+      render json: task
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def task_params
