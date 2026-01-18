d6f3c107b886f4448368bc3db4402456ea79c020
sahil-incubyte <sahil@incubyte.co>
2026-01-18 18:09:34 +0530

    Add authorization test preventing cross-user task access

---
+++ spec/requests/task_authorization_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Task Authorization", type: :request do
+  let!(:user_a) do
+    User.create!(
+      name: "User A",
+      email: "a@test.com",
+      password: "password123",
+      password_confirmation: "password123"
+    )
+  end
+
+  let!(:user_b) do
+    User.create!(
+      name: "User B",
+      email: "b@test.com",
+      password: "password123",
+      password_confirmation: "password123"
+    )
+  end
+
+  let!(:task_a) do
+    Task.create!(
+      title: "User A Task",
+      user: user_a
+    )
+  end
+
+  let(:token_b) { JsonWebToken.encode(user_id: user_b.id) }
+
+  it "prevents accessing another user's task" do
+    get "/tasks/#{task_a.id}", headers: {
+      "Authorization" => "Bearer #{token_b}"
+    }
+
+    expect(response).to have_http_status(:not_found)
+  end
+end
