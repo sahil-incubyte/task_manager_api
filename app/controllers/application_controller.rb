2d1a2277efbe2fa31b92b2c47cfa46c98de87098
sahil-incubyte <sahil@incubyte.co>
2026-01-18 17:56:22 +0530

    Define authenticate_request before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+  def authenticate_request
+    # For now, do nothing
+    # We will implement JWT verification in the next step
+  end
 end
